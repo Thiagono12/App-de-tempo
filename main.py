1bdf4448b4ae65ddbe467ee29749bc5ffff0bede
Thiago Lopes <thiagonobrelopes@gmail.com>
2025-12-14 21:59:49 -0300

    inserindo as libs necessarias e anexando no programa main

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-#teste dnv 
+#Vamos começar importando as libs necessarias
+#Let's start by importing the required libs
+import sys 
+import requests
+from PyQt5.QtWidgets import (QApplication, QWidget, QLabel,
+                              QLineEdit, QPushButton, QVBoxLayout)
+from PyQt5.QtCore import Qt
+
+
+class AppClima(QWidget):
+    def __init__(self):
+        super().__init__()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    App_Clima = AppClima()
+    App_Clima.show()
+    sys.exit(app.exec_())
