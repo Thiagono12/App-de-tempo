1d76922a693663f6c547ba5a9d938652001f841d
Thiago Lopes <thiagonobrelopes@gmail.com>
2025-12-14 23:05:16 -0300

    definindo a planta do app clima

--- main.py
+++ main.py
@@ -10,9 +10,17 @@ from PyQt5.QtCore import Qt
 class AppClima(QWidget):
     def __init__(self):
         super().__init__()
+        self.cidade_label = QLabel("Digite o nome da cidade: ", self)
+        self.cidade_input = QLineEdit(self)
+        self.get_clima_button = QPushButton("Veja o clima!", self)
+        self.temperatura_label = QLabel("15°C", self)
+        self.emoji_label = QLabel("☀️", self)
+        self.descricao_label = QLabel("Ensolarado", self)
+
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    App_Clima = AppClima()
+    App_Clima = AppClima() 
     App_Clima.show()
     sys.exit(app.exec_())
